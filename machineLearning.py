daa1fd9bd6d24e5e7ae5ba0c58b8d4dd6e71b7b8
liel20946 <liel999999@gmail.com>
2021-05-26 21:00:15 +0300

    getting data plus visualization, also normalzing the data is done

--- machineLearning.py
+++ machineLearning.py
@@ -3,16 +3,19 @@ from scipy import stats
 import yfinance as yf
 import pandas as pd
 import plotly.graph_objs as go
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
 
 
 #data = yf.download(tickers='AMZN', period='1y', interval='1h') #IN CASE WE WANT TO CHANGE THE INTERVAL CAN BE USED THAT WAY
 
 tickerData = yf.Ticker('AMZN')
-startDate = '2015-04-01' # as strings
+startDate = '2001-04-01' # as strings
 endDate = '2021-05-25' # as strings
 
 # Create historic data dataframe and fetch the data for the dates given. 
 data= tickerData.history(start = startDate, end = endDate)
+
 
 #declare figure
 fig = go.Figure()
@@ -43,16 +46,70 @@ fig.update_xaxes(
 )
 
 #Show
-fig.show()
-
-#print(df.head())
+#fig.show()
 
 
 
-#branch test
-#test 2
-#test
-#lat test
+high_prices = data.loc[:,'High']
+low_prices = data.loc[:,'Low']
+mid_prices = (high_prices+low_prices)/2.0
+
+train_data = mid_prices[:2500]
+test_data = mid_prices[2500:]
+
+scaler = MinMaxScaler()
+train_data = train_data.values.reshape(-1,1)
+test_data = test_data.values.reshape(-1,1)
+
+# Train the Scaler with training data and smooth data
+smoothing_window_size = 800
+for di in range(0,2400,smoothing_window_size):
+    scaler.fit(train_data[di:di+smoothing_window_size,:])
+    train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
+
+# You normalize the last bit of remaining data
+scaler.fit(train_data[di+smoothing_window_size:,:])
+train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
+
+# Now perform exponential moving average smoothing
+# So the data will have a smoother curve than the original ragged data
+EMA = 0.0
+gamma = 0.1
+for ti in range(2500):
+  EMA = gamma*train_data[ti] + (1-gamma)*EMA
+  train_data[ti] = EMA
+
+# Used for visualization and test purposes
+all_mid_data = np.concatenate([train_data,test_data],axis=0)
+
+#showing the graph using plt
+plt.figure(figsize = (18,9))
+plt.plot(range(data.shape[0]),all_mid_data,color='b',label='True')
+plt.xlabel('Date')
+plt.ylabel('Mid Price')
+plt.legend(fontsize=18)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 """
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
